e76274e211cc9becbeb922d6ec13ee96d2bcbd3a
Jyotiraditya Dhalmahapatra <Jyotiraditya.Dhalmahapatra@expertextend.de>
2021-03-15 18:04:16 +0530

    Update Microservice Folder
    1. organized in folder structure
    2. Added Small example project

---
+++ Microservices/Example/SimpleMicroservices/UserService/Controllers/UserController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserService.Database.Entity;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace UserService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        readonly DatabaseContext dbContext;
+
+        public UserController()
+        {
+            dbContext = new DatabaseContext();
+        }
+        // GET: api/<UserController>
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            return dbContext.Users.ToList();
+        }
+
+        // GET api/<UserController>/5
+        [HttpGet("{id}")]
+        public User Get(int id)
+        {
+            return dbContext.Users.Find(id);
+        }
+
+        // POST api/<UserController>
+        [HttpPost]
+        public IActionResult Post([FromBody] User userModel)
+        {
+            try
+            {
+                dbContext.Users.Add(userModel);
+                dbContext.SaveChanges();
+                return StatusCode(StatusCodes.Status201Created, userModel);
+            }
+            catch(Exception exp)
+            {
+                Console.WriteLine("Exception is", exp.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError, exp.Message);
+            }
+        }
+
+        //// PUT api/<UserController>/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //    try
+        //}
+
+        //// DELETE api/<UserController>/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
---
+++ Microservices/Example/SimpleMicroservices/UserService/Database/DatabaseContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserService.Database.Entity;
+
+namespace UserService
+{
+    public class DatabaseContext :DbContext
+    {
+        public DatabaseContext() : base()
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Data Source=(LocalDB)\\.;Initial Catalog=UserMicroservice;persist security info=True;Integrated Security=True");
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
